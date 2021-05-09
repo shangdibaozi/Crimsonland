8b0bacc04f58ba3ee992f3919980784d68b2161b
shangdibaozi <shangdibaozi@qq.com>
2021-05-09 20:14:33 +0800

    UI插件

--- assets/Scripts/UI/UIBase/UIBase.ts
+++ assets/Scripts/UI/UIBase/UIBase.ts
@@ -1,11 +1,14 @@
 
 import { _decorator, Component, Node, view } from 'cc';
+import { UIHelper } from './UIHelper';
 const { ccclass, property } = _decorator;
 
 @ccclass('UIBase')
 export class UIBase extends Component {
-
+    private static uiHelper = new UIHelper();
     private static perfectScale = -1;
+
+    private _hasBindUI : boolean = false;
     
     protected adapterSize() {
         if(UIBase.perfectScale === -1) {
@@ -20,5 +23,20 @@ export class UIBase extends Component {
             }
         }
         this.node.setScale(UIBase.perfectScale, UIBase.perfectScale, 1);
+    }
+
+    __preload() {
+        this.bindUI();
+    }
+
+    bindUI() {
+        if(!this._hasBindUI) {
+            this._hasBindUI = true;
+            UIBase.uiHelper.bindUI(this.node, this);
+        }
+    }
+
+    show(...arg : any[]) {
+        this.node.active = true;
     }
 }
