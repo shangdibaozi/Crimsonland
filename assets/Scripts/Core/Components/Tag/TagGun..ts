4cb4a52b0ea83a68be67d67133e38b19067d7f8f
shangdibaozi <shangdibaozi@qq.com>
2021-05-10 18:56:44 +0800

    控制场上掉落枪的数量

---
+++ assets/Scripts/Core/Components/Tag/TagGun..ts
@@ -0,0 +1,9 @@
+import { ecs } from "../../../Libs/ECS";
+
+@ecs.register('TagGun')
+export class TagGun extends ecs.IComponent {
+
+    reset() {
+
+    }
+}
--- assets/Scripts/Core/Systems/Item/ItemFactory.ts
+++ assets/Scripts/Core/Systems/Item/ItemFactory.ts
@@ -3,9 +3,16 @@ import { Global } from "../../../Global";
 import { ecs } from "../../../Libs/ECS";
 import { Util } from "../../../Util";
 import { MonsterDead } from "../../Components/MonsterDead";
+import { TagGun } from "../../Components/Tag/TagGun.";
 import { EntityFactory } from "../EntityFactory";
 
 export class ItemFactory extends ecs.ComblockSystem {
+
+    gunGroup!: ecs.Group;
+
+    init() {
+        this.gunGroup = ecs.createGroup(ecs.allOf(TagGun));
+    }
 
     filter(): ecs.IMatcher {
         return ecs.allOf(MonsterDead);
@@ -15,13 +22,17 @@ export class ItemFactory extends ecs.ComblockSystem {
         entities.forEach(ent => {
             let pos = ent.get(MonsterDead).pos;
 
-            let gunNode = instantiate(Util.randomChoice(Global.gunCfg!.gunInfos).gun);
-            gunNode.parent = Global.gameWorld!.avatarLayer;
-            gunNode.setPosition(pos);
+            if(Math.random() <= 0.3 && this.gunGroup.count < 3) {
+                let gunNode = instantiate(Util.randomChoice(Global.gunCfg!.gunInfos).gun);
+                gunNode.parent = Global.gameWorld!.avatarLayer;
+                gunNode.setPosition(pos);
+    
+                let itemEnt = EntityFactory.createItemEnt();
+                itemEnt.ECSNode.val = gunNode;
+                itemEnt.Lifetime.time = 40;
 
-            let itemEnt = EntityFactory.createItemEnt();
-            itemEnt.ECSNode.val = gunNode;
-            itemEnt.Lifetime.time = 10;
+                itemEnt.add(TagGun);
+            }
 
             ent.destroy();
         });
