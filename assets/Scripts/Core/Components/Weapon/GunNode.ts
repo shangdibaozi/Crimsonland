ce188fb100c72893800b6045d7644734ba7e9600
shangdibaozi <shangdibaozi@qq.com>
2021-05-21 22:51:35 +0800

    枪优化

--- assets/Scripts/Core/Components/Weapon/GunNode.ts
+++ assets/Scripts/Core/Components/Weapon/GunNode.ts
@@ -1,12 +1,16 @@
 import { ecs } from "../../../Libs/ECS";
-import { Node, UITransform, Animation, Vec3 } from "cc";
+import { Node, UITransform, Animation, v3 } from "cc";
 import { ECSNode } from "../ECSNode";
 
+let pos = v3();
 @ecs.register('GunNode')
 export class GunNode extends ecs.IComponent {
     set root(node: Node) {
         if(node) {
-            node.setPosition(Vec3.ZERO);
+            let body = node.getChildByName('Sprite')!;
+            body.getPosition(pos);
+            pos.y = 0;
+            body.setPosition(pos);
             node.getComponent(Animation)!.stop();
             this.ent.add(ECSNode).val = node;
             this.shadow = node.getChildByName('Shadow');
