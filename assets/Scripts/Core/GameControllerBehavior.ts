e492fc0532331d25bbd3358837b10a7ff6aa5af3
shangdibaozi <shangdibaozi@qq.com>
2022-01-02 00:16:53 +0800

    更新引擎

--- assets/Scripts/Core/GameControllerBehavior.ts
+++ assets/Scripts/Core/GameControllerBehavior.ts
@@ -8,29 +8,6 @@ import { RootSystem } from './Systems/RootSystem';
 import { SysUtil } from './Systems/SysUtil';
 const { ccclass, property } = _decorator;
 
-let start = 11;
-
-function f(key: string): any {
-    console.log("key: ", key)
-    return function(e: any, b:any, c:any) {
-        
-        for(let k in e) {
-            e[k] = start++;
-            console.log(k);
-        }
-        // e[b] = start++;
-        console.log(e[b])
-    }
-}
-
-@f('T')
-class Test {
-    // @f('A')
-    static A: number;
-    // @f('B')
-    static B: number;
-}
-
 @ccclass('GameControllerBehavior')
 export class GameControllerBehavior extends Component {
     rootSystem: RootSystem | null = null;
@@ -46,9 +23,6 @@ export class GameControllerBehavior extends Component {
         window['ecs'] = ecs;
         // @ts-ignore
         window['ObjPool'] = ObjPool;
-
-        console.log(Test.A);
-        console.log(Test.B);
     }
 
     stopEcs() {
